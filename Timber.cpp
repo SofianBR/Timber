2dd1955dcf27c92aed75bf38d4bf7e40dacee8bf
Sofian <sofian.benhamman@gmail.com>
2021-03-27 18:36:50 +0100

    Creado la ventana (modo pantalla completa), añadido el background, el árbol, los sonidos, sprites y fuentes.

--- Timber.cpp
+++ Timber.cpp
@@ -1,19 +1,55 @@
-// Timber.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-
+// Include graphic headers from SFML library, so we can use those functions.
+#include <SFML/Graphics.hpp>
+	
+// This is where my game starts.
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Creating a video game mode object.
+	sf::VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game.
+	sf::RenderWindow window(vm, "Timer!!!", sf::Style::Fullscreen);
+
+	// Background
+	// Create a texture to hold a graphic on the GPU.
+	sf::Texture textureBackground;
+	// Load a graphic into the texture;
+	textureBackground.loadFromFile("graphics/background.png");
+	// Create the background sprite.
+	sf::Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	// This is another way to set the position.
+	// Sprite.setPosition function takes 2 floats or a Vector2f (which contains 2 floats).
+	//sf::Vector2f initialPos = { 0, 0 };
+	spriteBackground.setPosition(0, 0);
+
+	// Tree
+	// Create the tree texture.
+	sf::Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	sf::Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	while (window.isOpen())
+	{
+
+		// If the player hits 'Escape', will close the window (and the game).
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+
+		// Clear everything from the last frame.
+		window.clear();
+
+		window.draw(spriteBackground);
+		window.draw(spriteTree);
+
+		// Updating the frame with everything I draw.
+		window.display();
+	}
+
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
